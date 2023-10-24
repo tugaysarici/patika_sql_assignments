7336adee069bc58a62ab4eea4f4da98b27bde50a
tugaysarici <tugaysarici@gmail.com>
2023-10-24 14:57:23 +0300

    hw4 added

---
+++ exercises/06_distinct_ve_count.sql
@@ -0,0 +1,36 @@
+SELECT rental_rate FROM film;
+
+SELECT DISTINCT rental_rate FROM film;
+
+SELECT DISTINCT replacement_cost FROM film;
+
+SELECT * FROM actor
+WHERE first_name = 'Penelope';
+
+SELECT COUNT(*) FROM actor
+WHERE first_name = 'Penelope';
+
+SELECT * FROM actor
+WHERE first_name LIKE 'A%';
+
+SELECT COUNT(*) FROM actor
+WHERE first_name LIKE 'A%';
+
+SELECT COUNT(first_name) FROM actor
+WHERE first_name LIKE 'A%';
+
+SELECT * FROM actor;
+
+SELECT COUNT(*) FROM actor;
+
+SELECT DISTINCT first_name FROM actor;
+
+SELECT COUNT(DISTINCT first_name) FROM actor;
+
+SELECT * FROM film;
+
+SELECT COUNT(*) FROM film;
+
+SELECT DISTINCT length FROM film;
+
+SELECT COUNT(DISTINCT length) FROM film;
